9e684e8986111eceaab5a101f1fe90aacf3aaf67
Scytale <federicoboainain@gmail.com>
2022-02-10 16:20:10 -0300

    experiencia-lab con binding en json

--- src/app/component/experiencia-laboral/experiencia-laboral.component.ts
+++ src/app/component/experiencia-laboral/experiencia-laboral.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/service.service';
 
 @Component({
   selector: 'app-experiencia-laboral',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia-laboral.component.css']
 })
 export class ExperienciaLaboralComponent implements OnInit {
-
-  constructor() { }
+  experiencia_list:any;
+  constructor(private datosPortfolio: DatosService ) { }
 
   ngOnInit(): void {
-  }
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.experiencia_list=data.experiencia
+      console.log(data.experiencia)
+  });
+}
 
 }
